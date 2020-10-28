532f264fa02507b9672d21523ce088ef6c0df772
Young <young981@qq.com>
2020-10-28 15:06:07 +0800

    project init

---
+++ src/test/java/com/aia/repairman/RepairmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aia.repairman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairmanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
